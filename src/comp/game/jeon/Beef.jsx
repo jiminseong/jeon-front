7d499596fd719d66c205c48b57b54dcbc386da52
jiminseong <iamjms4237@gmail.com>
2024-02-13 12:59:50 +0900

    second commit

---
+++ src/comp/game/jeon/Beef.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import beef from '../../../assets/img/beef.png';
+
+const Beef = () => {
+    return (
+        <div>
+            <img src={beef}></img>
+        </div>
+    );
+};
+
+export default Beef;
---
+++ src/comp/game/jeon/Perilla.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import perilla from '../../../assets/img/perillaleaf.png';
+
+const Perilla = () => {
+    return (
+        <div>
+            <img src={perilla}></img>
+        </div>
+    );
+};
+
+export default Perilla;
